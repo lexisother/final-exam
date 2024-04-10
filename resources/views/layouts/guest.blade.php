bb5b5501d54f7e309ca68b958e333775bcc7750c
Alyxia Sother <alyxia@riseup.net>
2024-04-10 14:28:39 +0200

    [views/layouts/guest] Fix assets locations

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
       rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.scss', 'resources/ts/app.ts'])
   </head>
   <body class="font-sans text-gray-900 antialiased">
     <div
