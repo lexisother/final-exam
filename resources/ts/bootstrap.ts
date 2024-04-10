8700ae0e9babbbf4b1fb0415840917b5c1ba0c65
Alyxia Sother <alyxia@riseup.net>
2024-04-10 14:28:36 +0200

    [ts/bootstrap] Enable navbar interactivity

--- resources/ts/bootstrap.ts
+++ resources/ts/bootstrap.ts
@@ -1,4 +1,23 @@
 import axios from 'axios';
-window.axios = axios;
 
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let bodyhandler: (this: HTMLBodyElement) => void;
+const container = document.querySelector('#container')!;
+const button = document.querySelector('#sidebar-button')!;
+const sidebar = document.querySelector('#sidebar')!;
+
+button.addEventListener('click', function (this: HTMLButtonElement) {
+    document.body.classList.add('overflow-hidden');
+    sidebar.classList.remove('-translate-x-full');
+    sidebar.classList.add('transform-none');
+
+    bodyhandler = function (this: HTMLBodyElement) {
+        document.body.classList.remove('overflow-hidden');
+        sidebar.classList.remove('transform-none');
+        sidebar.classList.add('-translate-x-full');
+        container.removeEventListener('click', bodyhandler);
+    };
+    container.addEventListener('click', bodyhandler);
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,27 +68,6 @@
 </body>
 </html>
 
-{{--<script>--}}
-{{--    let bodyhandler: (this: HTMLBodyElement) => void;--}}
-{{--    const container = document.querySelector('#container')!;--}}
-{{--    const button = document.querySelector('#sidebar-button')!;--}}
-{{--    const sidebar = document.querySelector('#sidebar')!;--}}
-
-{{--    button.addEventListener('click', function (this: HTMLButtonElement) {--}}
-{{--        document.body.classList.add('overflow-hidden');--}}
-{{--        sidebar.classList.remove('-translate-x-full');--}}
-{{--        sidebar.classList.add('transform-none');--}}
-
-{{--        bodyhandler = function (this: HTMLBodyElement) {--}}
-{{--            document.body.classList.remove('overflow-hidden');--}}
-{{--            sidebar.classList.remove('transform-none');--}}
-{{--            sidebar.classList.add('-translate-x-full');--}}
-{{--            container.removeEventListener('click', bodyhandler);--}}
-{{--        };--}}
-{{--        container.addEventListener('click', bodyhandler);--}}
-{{--    });--}}
-{{--</script>--}}
-
 {{--<style lang="scss">--}}
 {{--    :root {--}}
 {{--        --accent: 136, 58, 234;--}}
