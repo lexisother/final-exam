0cdc58584ffee87137ad1060d89673a0d822eb09
Alyxia Sother <alyxia@riseup.net>
2024-04-10 14:28:49 +0200

    [routes/web] Fix middleware for subroutes

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('dashboard')->group(function() {
-    Route::get('/', fn () => view('dashboard'))->name('dashboard');
-    Route::get('/strippenkaarten', fn () => view('stripcards'));
-    Route::get('/verslagen', fn () => view('reports'));
-    Route::get('/leerlingen', fn () => view('students'));
-})->middleware(['auth', 'verified']);
-
-Route::prefix('api')->group(function() {
-    Route::prefix('cards')->group(function() {
-        Route::post('/create', [CardController::class, 'create']);
-        Route::get('/delete/{id}', [CardController::class, 'delete']);
+Route::middleware(['auth', 'verified'])
+    ->prefix('dashboard')
+    ->group(function() {
+        Route::get('/', fn () => view('dashboard'))->name('dashboard');
+        Route::get('/strippenkaarten', fn () => view('stripcards'));
+        Route::get('/verslagen', fn () => view('reports'));
+        Route::get('/leerlingen', fn () => view('students'));
     });
-})->middleware(['auth', 'verified']);
+
+Route::middleware(['auth', 'verified'])
+    ->prefix('api')
+    ->group(function() {
+        Route::prefix('cards')->group(function() {
+            Route::post('/create', [CardController::class, 'create']);
+            Route::get('/delete/{id}', [CardController::class, 'delete']);
+        });
+    });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
